efd9d64465f0949213dfeeba35dabd47af74b30a
Connor Hicks <connorjhicks@gmail.com>
2021-02-21 17:56:48 -0500

    added Schedule type and watcher that manages Schedules

---
+++ rt/tester/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/suborbital/reactr/rt"
+)
+
+func main() {
+	r := rt.New()
+
+	r.Handle("print", &printJob{})
+
+	r.Do(rt.NewJob("print", "start")).Discard()
+
+	r.Schedule(rt.Every(5, func() rt.Job {
+		return rt.NewJob("print", "every 5")
+	}))
+
+	r.Schedule(rt.After(7, func() rt.Job {
+		return rt.NewJob("print", "after 7")
+	}))
+
+	time.Sleep(time.Second * 25)
+}
+
+type printJob struct{}
+
+func (p *printJob) Run(job rt.Job, ctx *rt.Ctx) (interface{}, error) {
+	fmt.Println(job.String())
+	return nil, nil
+}
+
+func (p *printJob) OnChange(c rt.ChangeEvent) error { return nil }
