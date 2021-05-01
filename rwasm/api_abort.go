997a1bbb2af0fc219f9d863d77d9278a24ff1d6b
cohix <connor@suborbital.dev>
2021-05-01 16:24:32 -0400

    further abort updates

--- rwasm/api_abort.go
+++ rwasm/api_abort.go
@@ -3,8 +3,6 @@ package rwasm
 import (
 	"fmt"
 
-	"github.com/pkg/errors"
-	"github.com/suborbital/reactr/rt"
 	"github.com/wasmerio/wasmer-go/wasmer"
 )
 
@@ -14,9 +12,8 @@ func abortHandler() *HostFn {
 		fileName := args[1].I32()
 		lineNumber := args[2].I32()
 		columnNumber := args[3].I32()
-		ident := args[4].I32()
 
-		abort(message, fileName, lineNumber, columnNumber, ident)
+		abort(message, fileName, lineNumber, columnNumber)
 
 		return nil, nil
 	}
@@ -24,16 +21,8 @@ func abortHandler() *HostFn {
 	return newHostFn("abort", 4, false, fn)
 }
 
-func abort(message int32, fileName int32, lineNumber int32, columnNumber int32, ident int32) {
-	inst, err := instanceForIdentifier(ident, false)
-	if err != nil {
-		logger.Error(errors.Wrap(err, "[rwasm] alert: invalid identifier used, potential malicious activity"))
-		return
-	}
+func abort(message int32, fileName int32, lineNumber int32, columnNumber int32) {
+	errMsg := fmt.Sprintf("instance aborted: line: %d, col: %d", lineNumber, columnNumber)
 
-	errMsg := fmt.Sprintf("aborted: line: %d, col: %d", lineNumber, columnNumber)
-
-	runErr := rt.RunErr{Code: 1, Message: errMsg}
-
-	inst.errChan <- runErr
+	logger.ErrorString(errMsg)
 }
