a4e6ed3e3aa51d4cff663afb9bfc1dd630eb0d65
cohix <connor@suborbital.dev>
2021-06-14 22:43:57 -0400

    add cache functions to AS library

--- rwasm/wasmtest/interop_test.go
+++ rwasm/wasmtest/interop_test.go
@@ -49,3 +49,45 @@ func TestWasmCacheGetSetSwiftToRust(t *testing.T) {
 		t.Error(fmt.Errorf("did not get expected output"))
 	}
 }
+
+func TestWasmCacheGetSetSwiftToAS(t *testing.T) {
+	setJob := rt.NewJob("swift-set", "very important")
+	getJob := rt.NewJob("as-get", "")
+
+	_, err := sharedRT.Do(setJob).Then()
+	if err != nil {
+		t.Error(errors.Wrap(err, "failed to set cache value"))
+		return
+	}
+
+	r2, err := sharedRT.Do(getJob).Then()
+	if err != nil {
+		t.Error(errors.Wrap(err, "failed to get cache value"))
+		return
+	}
+
+	if string(r2.([]byte)) != "very important" {
+		t.Error(fmt.Errorf("did not get expected output"))
+	}
+}
+
+func TestWasmCacheGetSetASToRust(t *testing.T) {
+	setJob := rt.NewJob("as-set", "very important")
+	getJob := rt.NewJob("rust-get", "")
+
+	_, err := sharedRT.Do(setJob).Then()
+	if err != nil {
+		t.Error(errors.Wrap(err, "failed to set cache value"))
+		return
+	}
+
+	r2, err := sharedRT.Do(getJob).Then()
+	if err != nil {
+		t.Error(errors.Wrap(err, "failed to get cache value"))
+		return
+	}
+
+	if string(r2.([]byte)) != "very important" {
+		t.Error(fmt.Errorf("did not get expected output"))
+	}
+}
