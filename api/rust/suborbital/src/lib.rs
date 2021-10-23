a57411be0a18bae93d2277891cf4cfb3c6c68085
Florian Fromm <flrn.frmm@gmail.com>
2021-10-23 11:49:56 +0200

    Refactor STATE to contain Option<Runnable>
    Removed `DefaultRunnable`.
    Runnable is now a Option and is default set to `None`.

--- api/rust/suborbital/src/lib.rs
+++ api/rust/suborbital/src/lib.rs
@@ -13,16 +13,14 @@ pub mod util;
 /// This file represents the Rust "API" for Reactr Wasm runnables. The functions defined herein are used to exchange
 /// data between the host (Reactr, written in Go) and the Runnable (a Wasm module, in this case written in Rust).
 
-use runnable::default_runnable::DefaultRunnable;
-
 /// State struct to hold our dynamic Runnable
 struct State <'a> {
     ident: i32,
-    runnable: &'a dyn runnable::Runnable
+    runnable: Option<&'a dyn runnable::Runnable>
 }
 
 /// The state that holds the user-provided Runnable and the current ident
 static mut STATE: State = State {
     ident: 0,
-    runnable: &DefaultRunnable::new()
+    runnable: None
 };
