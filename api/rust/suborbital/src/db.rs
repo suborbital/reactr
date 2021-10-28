61b274ebc74ea7d5198615fad3fd192a6ccabea1
cohix <connor@suborbital.dev>
2021-10-28 11:24:05 -0400

    add comments to select and insert functions

--- api/rust/suborbital/src/db.rs
+++ api/rust/suborbital/src/db.rs
@@ -10,6 +10,11 @@ extern {
 	fn db_exec(query_type: i32, name_ptr: *const u8, name_size: i32, ident: i32) -> i32;
 }
 
+// insert executes the pre-loaded database query with the name <name>,
+// and passes the arguments defined by <args>
+//
+// the return value is the inserted auto-increment ID from the query result, if any,
+// formatted as JSON with the key `lastInsertID`
 pub fn insert(name: &str, args: Vec<QueryArg>) -> Result<Vec<u8>, runnable::HostErr> {
 	for a in args {
 		ffi::add_var(a.name.as_str(), a.value.as_str())
@@ -21,6 +26,10 @@ pub fn insert(name: &str, args: Vec<QueryArg>) -> Result<Vec<u8>, runnable::Host
 	ffi::result(result_size)
 }
 
+// insert executes the pre-loaded database query with the name <name>,
+// and passes the arguments defined by <args>
+//
+// the return value is the query result formatted as JSON, with each column name as a top-level key
 pub fn select(name: &str, args: Vec<QueryArg>) -> Result<Vec<u8>, runnable::HostErr> {
 	for a in args {
 		ffi::add_var(a.name.as_str(), a.value.as_str())
