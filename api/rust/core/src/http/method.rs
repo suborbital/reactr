a98623c5763edb52e67d44304e3ddbaffadde3f6
cohix <connor@suborbital.dev>
2021-11-06 16:12:45 -0400

    fix methods

--- api/rust/core/src/http/method.rs
+++ api/rust/core/src/http/method.rs
@@ -11,13 +11,13 @@ pub enum Method {
 impl From<Method> for i32 {
 	fn from(field_type: Method) -> Self {
 		match field_type {
-			GET => 0,
-			HEAD => 1,
-			OPTIONS => 2,
-			POST => 3,
-			PUT => 4,
-			PATCH => 5,
-			DELETE => 6,
+			Method::GET => 0,
+			Method::HEAD => 1,
+			Method::OPTIONS => 2,
+			Method::POST => 3,
+			Method::PUT => 4,
+			Method::PATCH => 5,
+			Method::DELETE => 6,
 		}
 	}
 }
