48d912082e29962d683a628a2a77d688015eb158
Ryan P <ryan@confluent.io>
2022-02-18 08:32:29 -0500

    Add HTTP port rules to HTTPFilter (#185) (#210)

--- util/util_test.go
+++ util/util_test.go
@@ -11,3 +11,15 @@ func TestGenerateResultID(t *testing.T) {
 		t.Errorf("id has length %d, expected 24", len(id))
 	}
 }
+
+func TestContainsInt(t *testing.T) {
+	container := []int{1, 2, 3, 4}
+
+	if !ContainsInt(3, container) {
+		t.Errorf("expected value not found in container")
+	}
+
+	if ContainsInt(5, container) {
+		t.Errorf("should not have found value in container")
+	}
+}
